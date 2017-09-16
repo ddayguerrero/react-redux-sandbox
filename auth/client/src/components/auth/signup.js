509ba7cb8e71a54056f1934fbe68214ffc13fbef
Darrel Guerrero <ddayy.guerrero@hotmail.com>
2017-09-15 23:16:38 -0400

    Add sign-up component

---
+++ auth/client/src/components/auth/signup.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
+class Signup extends Component {
+  render(){
+    return (
+      <div>Signup</div>
+    );
+  }
+}
+
+export default Signup;
